26d242bb4d32dd937c0c7b50e7637d6c3dfd2374
Sergey Indenbom <dzorogh@gmail.com>
2023-06-15 14:39:43 +0300

    Initial commit

---
+++ index.ts
@@ -0,0 +1,25 @@
+// const urlRegex = new
+// RegExp('(refreshce|yclid|gclid|cx|ie|cof|siteurl|zanpid|origin|utm_(source|campaign|medium)|fb(cl)?id|fbclid|mr:[A-z]+ref(id|src))');
+const urlRegex = new RegExp('(yclid|gclid|utm_(source|campaign|medium))');
+
+export async function stripUtmFromRequest(request: Request) {
+  let url = new URL(request.url)
+
+  url = await normalizeUrl(url)
+
+  return new Request(url, request)
+}
+
+async function normalizeUrl(url: URL) {
+  let deleteKeys = []
+
+  for (const key of url.searchParams.keys()) {
+    if (key.match(urlRegex)) {
+      deleteKeys.push(key)
+    }
+  }
+
+  deleteKeys.forEach(k => url.searchParams.delete(k))
+
+  return url
+}

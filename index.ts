931a906c77f2a3730c90c68dff78667edd941b8a
Sergey Indenbom <dzorogh@gmail.com>
2023-06-22 10:06:02 +0300

    feat: add utm_term

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 // const urlRegex = new
 // RegExp('(refreshce|yclid|gclid|cx|ie|cof|siteurl|zanpid|origin|utm_(source|campaign|medium)|fb(cl)?id|fbclid|mr:[A-z]+ref(id|src))');
-const urlRegex = new RegExp('(yclid|gclid|utm_(source|campaign|medium))');
+const urlRegex = new RegExp('(yclid|gclid|utm_(source|campaign|medium|term))');
 
 export async function stripUtmFromRequest(request: Request) {
   let url = new URL(request.url)
